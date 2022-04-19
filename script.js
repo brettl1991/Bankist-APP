9f4180ccbfb0c1b6c0af4fefda08b1027dc859bf
brettl1991 <brettl1991@gmail.com>
2022-04-19 16:48:30 +0100

    Implementing login for current user

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html); //method accept 2 strings:1st the position in which we want to attach the HTML, 2nd is  a string containing the HTML that we want to insert, just we wrote above in const html
   });
 };
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
 // console.log(containerMovements.innerHTML); //with this we can see the whole html elements that we passed in above in the console: all the 8 movements from account1
 
 //Compute usernames for each account
@@ -100,33 +100,65 @@ console.log(accounts); //from console we can see results
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => (acc += mov), 0);
   //adding to the balance value html element:
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance} €`;
 };
 
-calcDisplayBalance(account1.movements);
+// calcDisplayBalance(account1.movements);
 
 //calc income, outcome and interest summary
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (account) {
+  const incomes = account.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}₤`;
+  labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = account.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}₤`;
+  labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = account.movements
     .filter(mov => mov > 0)
-    .map(mov => (mov * 1.2) / 100)
+    .map(mov => (mov * account.interestRate) / 100)
     //if the intrest below one, not going to be added to the intrest sum so:
     .filter((mov, i, arr) => {
-      console.log(arr);
+      // console.log(arr);
       return mov >= 1;
     })
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumInterest.textContent = `${interest}₤`;
+  labelSumInterest.textContent = `${interest}€`;
 };
 
-calcDisplaySummary(account1.movements);
+// calcDisplaySummary(account1.movements);
+
+//Implementing Login
+//Event Handler
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  //Prevent from from submitting
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  //check if pin is correct
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //?. optional chaining so instead of error we get back undefined if the login username not correct
+    //Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    //Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    //Display movements
+    displayMovements(currentAccount.movements);
+    //Display balance
+    calcDisplayBalance(currentAccount.movements);
+    //Display summary
+    calcDisplaySummary(currentAccount);
+  }
+});
