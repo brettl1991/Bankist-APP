849e2dd62aa4961137ecf81566650c02e3921388
brettl1991 <brettl1991@gmail.com>
2022-04-21 15:53:31 +0100

    Request a loan from a bank, only will get if the movement >= than 10% of the amount

--- script.js
+++ script.js
@@ -195,6 +195,23 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+//Request a loan from a bank, only will get if the movement >= than 10% of the amount
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //add positive movement to the account
+    currentAccount.movements.push(amount);
+
+    //Update UI
+    updateUI(currentAccount);
+  }
+
+  //clear input field
+  inputLoanAmount.value = '';
+});
+
 //Close account
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
